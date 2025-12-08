ea19f93b6ba1544abc5155e8bde7c2996c3689d4
Swirl86 <swesuzzi@gmail.com>
2025-12-08 08:57:23 +0100

    Updated main menu design with retro arcade style

--- PocketArcade/app/src/main/java/com/swirl/pocketarcade/menu/MenuFragment.kt
+++ PocketArcade/app/src/main/java/com/swirl/pocketarcade/menu/MenuFragment.kt
@@ -2,6 +2,7 @@ package com.swirl.pocketarcade.menu
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.swirl.pocketarcade.R
@@ -17,6 +18,7 @@ class MenuFragment : Fragment(R.layout.fragment_menu) {
         _binding = FragmentMenuBinding.bind(view)
 
         binding.btnTictactoe.setOnClickListener {
+            it.startAnimation(AnimationUtils.loadAnimation(requireContext(), R.anim.btn_bounce))
             findNavController().navigate(R.id.action_menu_to_playerSetup)
         }
     }
