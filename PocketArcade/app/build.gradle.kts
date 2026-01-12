2c9cecdb3b81200e560ff6dc74bfbe60ac70d159
Swirl86 <swesuzzi@gmail.com>
2026-01-12 08:36:49 +0100

    Add Glide dependency to Gradle

--- PocketArcade/app/build.gradle.kts
+++ PocketArcade/app/build.gradle.kts
@@ -68,6 +68,9 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
+    implementation(libs.glide)
+    ksp(libs.glide.compiler)
+
     // --- Testing ---
     testImplementation(libs.junit)
     testImplementation(libs.core.testing)
